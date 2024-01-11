3cd8a56cf7d62def7b7d7323840ef86de1f3a2dd
Giulia Fontana <fontanagiulia6@gmail.com>
2024-01-11 12:49:56 +0100

    giulia - remove default content from appcss and add test content in app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,7 +4,11 @@ import GETLuoghiAttrazione from './componenti/GetData'
 function App() {
   return (
     <div>
-      
+      <h1>Ciao sono io</h1>
+      <nav>
+        <a href='#'>prova</a>
+        <a>ciao</a>
+      </nav>
     </div>
    
   );
--- Frontend/src/componenti/Home.js
+++ Frontend/src/componenti/Home.js
@@ -12,6 +12,8 @@ function Home(){
     }
 
     return{
-
+        
     }
+    
+    
 }
