2e63ddbc07f27ec5a75d9e8532266702c5f11f52
LinFlolin <datchanilinza@gmail.com>
2024-01-12 02:06:33 +0100

    #LuoghiCssFatti male

--- Frontend/src/Componenti/Pagine/LuoghiFigo.jsx
+++ Frontend/src/Componenti/Pagine/LuoghiFigo.jsx
@@ -1,78 +1,11 @@
 import React from 'react'
-
+import { Axios } from 'axios'
+import './LuoghiFigo.css'
 export const LuoghiFigo = () => {
   return (
-    <div>
-      
-      LuoghiFigo
-      <p>
-      Lorem ipsum dolor sit amet consectetur,
-         adipisicing elit. Voluptatem, ullam at vero 
-        tempore inventore vitae commodi consequuntur,
-        voluptas repellendus recusandae ut accusamus eum.
-         Modi esse, blanditiis provident rem nesciunt eius
-        itaque iste nisi harum veritatis vel. Unde ut
-         cupiditate laudantium quibusdam. Numquam aspernatur 
-         nostrum praesentium ducimus iure voluptates maxime
-         magnam eligendi facilis sequi magni sunt minus, 
-         sint in, libero dolores, ab incidunt eius odio 
-         reprehenderit maiores ipsum. Similique facilis 
-         at eveniet, consequuntur corrupti, ex nemo, 
-         reprehenderit architecto magni expedita ipsam
-          blanditiis molestiae beatae saepe nihil quidem 
-          adipisci? Accusamus quibusdam rerum similique 
-          vel quos natus, fugiat, soluta iste hic consequatur 
-          ducimus!Lorem ipsum dolor sit amet consectetur,
-         adipisicing elit. Voluptatem, ullam at vero 
-        tempore inventore vitae commodi consequuntur,
-        voluptas repellendus recusandae ut accusamus eum.
-         Modi esse, blanditiis provident rem nesciunt eius
-        itaque iste nisi harum veritatis vel. Unde ut
-         cupiditate laudantium quibusdam. Numquam aspernatur 
-         nostrum praesentium ducimus iure voluptates maxime
-         magnam eligendi facilis sequi magni sunt minus, 
-         sint in, libero dolores, ab incidunt eius odio 
-         reprehenderit maiores ipsum. Similique facilis 
-         at eveniet, consequuntur corrupti, ex nemo, 
-         reprehenderit architecto magni expedita ipsam
-          blanditiis molestiae beatae saepe nihil quidem 
-          adipisci? Accusamus quibusdam rerum similique 
-          vel quos natus, fugiat, soluta iste hic consequatur 
-          ducimus!Lorem ipsum dolor sit amet consectetur,
-         adipisicing elit. Voluptatem, ullam at vero 
-        tempore inventore vitae commodi consequuntur,
-        voluptas repellendus recusandae ut accusamus eum.
-         Modi esse, blanditiis provident rem nesciunt eius
-        itaque iste nisi harum veritatis vel. Unde ut
-         cupiditate laudantium quibusdam. Numquam aspernatur 
-         nostrum praesentium ducimus iure voluptates maxime
-         magnam eligendi facilis sequi magni sunt minus, 
-         sint in, libero dolores, ab incidunt eius odio 
-         reprehenderit maiores ipsum. Similique facilis 
-         at eveniet, consequuntur corrupti, ex nemo, 
-         reprehenderit architecto magni expedita ipsam
-          blanditiis molestiae beatae saepe nihil quidem 
-          adipisci? Accusamus quibusdam rerum similique 
-          vel quos natus, fugiat, soluta iste hic consequatur 
-          ducimus!Lorem ipsum dolor sit amet consectetur,
-         adipisicing elit. Voluptatem, ullam at vero 
-        tempore inventore vitae commodi consequuntur,
-        voluptas repellendus recusandae ut accusamus eum.
-         Modi esse, blanditiis provident rem nesciunt eius
-        itaque iste nisi harum veritatis vel. Unde ut
-         cupiditate laudantium quibusdam. Numquam aspernatur 
-         nostrum praesentium ducimus iure voluptates maxime
-         magnam eligendi facilis sequi magni sunt minus, 
-         sint in, libero dolores, ab incidunt eius odio 
-         reprehenderit maiores ipsum. Similique facilis 
-         at eveniet, consequuntur corrupti, ex nemo, 
-         reprehenderit architecto magni expedita ipsam
-          blanditiis molestiae beatae saepe nihil quidem 
-          adipisci? Accusamus quibusdam rerum similique 
-          vel quos natus, fugiat, soluta iste hic consequatur 
-          ducimus!
-      </p>
-
-    </div>
+    <div class="container">
+      <h1>My First Bootstrap Page</h1>
+      <p>This is some text.</p>
+    </div> 
   )
 }
