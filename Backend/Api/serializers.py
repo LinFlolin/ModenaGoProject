79440b32af5c5f184f55269040007f3ce39ed30b
Joannes Nicolò Zanfi <nicolo.zanfi@fitstic-edu.com>
2024-01-14 16:54:56 +0100

    Aggiungo "Mappa" model, viewset, serializer e router + superuser joannes

--- Backend/Api/serializers.py
+++ Backend/Api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import LuoghiAttrazione 
+from .models import LuoghiAttrazione, Mappa
 
 class LuoghiAttrazioneSerialiazer (serializers.ModelSerializer) :
     class Meta :
         model = LuoghiAttrazione 
         fields=('id','Titolo','Descrizione','Sfida')
+
+class MapSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Mappa
+        fields = ('Luogo', 'Latitudine', 'Longitudine')
