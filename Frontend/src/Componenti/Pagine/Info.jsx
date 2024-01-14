103b731f461a68a8fde1b8009b2d790d4804b3e2
LinFlolin <datchanilinza@gmail.com>
2024-01-14 18:37:25 +0100

    #InfoFetchData

--- Frontend/src/Componenti/Pagine/Info.jsx
+++ Frontend/src/Componenti/Pagine/Info.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-import {Mappa} from './Mappa'
-export const Info = () => {
-  return (
-    <div>
-      Info
-      
-    </div>
-  )
+import { Component } from 'react'
+import axios from 'axios'
+
+export class Info extends Component{
+
+    constructor(props){
+      super(props)
+      this.state = {
+          postsL : []
+      }
+  }
+  componentDidMount(){
+      axios.get('http://127.0.0.1:8000/api/LuoghiAtrazioni/','')
+      .then(response=> {
+          console.log(response)
+          this.setState({postsL : response.data})
+      })
+      .catch(error=>{
+          console.log(error)
+          this.setState({erroeMsg : 'error retreiving data'})
+      })
+
+  }
+  render (){
+      const { postsL } = this.state
+      return(
+        <div>
+          {
+            <ul>
+                {postsL.map(item =>(
+                    <li className="container " key={item.id}>
+                        Titolo:
+                        <be/>
+                        {item.Titolo}
+                        <br/>
+                        Descrizione:
+                        <br/>
+                        {item.Descrizione}
+                        <br/>                         
+                    </li>             
+                ))}
+            </ul>
+          }
+        </div>
+      )
+  }
 }
 
