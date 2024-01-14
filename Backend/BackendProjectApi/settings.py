550cd686baf2ab59e2aa0fea940564de8b954ea7
LinFlolin <datchanilinza@gmail.com>
2024-01-14 20:55:30 +0100

    #RipescareDB

--- Backend/BackendProjectApi/settings.py
+++ Backend/BackendProjectApi/settings.py
@@ -83,12 +83,8 @@ WSGI_APPLICATION = 'BackendProjectApi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'modena_go',
-        'USER': 'luca',
-        'PASSWORD': 'Luke',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
--- Frontend/src/Componenti/Pagine/Mappa.jsx
+++ Frontend/src/Componenti/Pagine/Mappa.jsx
@@ -1,43 +1,73 @@
-import React from 'react'
-// import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-import{
+import React, { Component } from 'react';
+import {
   APIProvider,
   Map,
   AdvancedMarker,
   Pin,
   InfoWindow
-}
-from '@vis.gl/react-google-maps'
+} from '@vis.gl/react-google-maps';
+import axios from 'axios';
 
-export function Mappa () {
-  const position = {
+export class Mappa extends Component {
+    constructor(props){
+      super(props)
+      this.state = {
+          postsL : []
+      }
+  }
+  componentDidMount(){
+    axios.get('http://127.0.0.1:8000/api/LuoghiAtrazioni/','')
+    .then(response=> {
+        console.log(response)
+        this.setState({postsL : response.data})
+    })
+    .catch(error=>{
+        console.log(error)
+        this.setState({erroeMsg : 'error retreiving data'})
+    })
+
+  }
+
+  state = {
+    open: false
+  };
+
+  position = {
     lat: 44.646420,
     lng: 10.926023
-     
   };
-  
-  return(
-    <APIProvider apiKey='AIzaSyBiqWG7fNUrBUtfJ_fjnDK02zcTME6oEeo'>
-    <div style={{height: "100vh"}}>
-      <Map zoom={15} center={position} mapId={'8b3c296d5e49a998'}>
-        <AdvancedMarker position={position} onClick={() => setOpen(true)}>
-          <Pin 
-            background={'gray'}
-          />
-        </AdvancedMarker>
-        {open &&(
-          <InfoWindow position={position} onCloseClick={() => setOpen(false)} >
-            <p>Heii Modena</p>
-          </InfoWindow>
-        )}     
 
-      </Map>
+  render() {
+    const { open } = this.state;
+    const { postsL } = this.state;
+
+    return (
       
-    </div>
-    </APIProvider>
-
-  )
-
+        <div style={{ height: "100vh" }}>
+          <APIProvider apiKey='AIzaSyBiqWG7fNUrBUtfJ_fjnDK02zcTME6oEeo'>
+            <Map zoom={15} center={this.position} mapId={'8b3c296d5e49a998'}>
+              <AdvancedMarker position={this.position} onClick={() => this.setState({ open: true })}>
+                <Pin background={'gray'} />
+              </AdvancedMarker>
+              {open && (
+                <InfoWindow position={this.position} onCloseClick={() => this.setState({ open: false })}>
+                  <p>
+                  <ul>
+                    {postsL.map(item =>(
+                        <div className="container " key={item.id}>
+                            {item.Titolo}
+                        </div>             
+                    ))}
+                 </ul>
+                  </p>
+                </InfoWindow>
+              )}
+            </Map>
+          </APIProvider>
+        </div>
+      
+    );
+  }
 }
 
-export default React.memo(Mappa)
+
