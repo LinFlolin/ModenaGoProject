3360afda04979b9e92b30964fbbb1d0de617ef87
Joannes Nicolò Zanfi <nicolo.zanfi@fitstic-edu.com>
2024-01-14 17:48:45 +0100

    added Mappa model in admin.py and 'id' field in serializers.py

--- Backend/Api/admin.py
+++ Backend/Api/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import LuoghiAttrazione 
+from .models import LuoghiAttrazione, Mappa
 
 class LuoghiAttrazioneAdmin (admin.ModelAdmin):
     list_display =('titolo','descrizione','sfida')
 
+class MappaAdmin(admin.ModelAdmin):
+    list_display =('luogo','latitudine','longitudine')
+
 # Register your models here.
-    admin.site.register(LuoghiAttrazione,)
+    admin.site.register(LuoghiAttrazione)
+    admin.site.register(Mappa)
 
