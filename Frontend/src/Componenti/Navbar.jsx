57db8d674078ae39544ddaebc6dc0c0312bb5021
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-01-18 12:26:23 +0100

    WNP Login

--- Frontend/src/Componenti/Navbar.jsx
+++ Frontend/src/Componenti/Navbar.jsx
@@ -1,4 +1,4 @@
-import {Link,NavLink} from 'react-router-dom'
+import {Link,NavLink,} from 'react-router-dom'
 import React,{useState} from 'react';
 import './Navbar.css'
 export const Navbar = ()=>{
