ac1cef5a1dbe1cd412d8614f9fc4cdc57c11e1ae
Luca Zannini <luca.zanninifitstic-edu.com>
2024-01-18 10:21:59 +0100

    commit per fare pull

--- Backend/BackendProjectApi/settings.py
+++ Backend/BackendProjectApi/settings.py
@@ -82,13 +82,9 @@ WSGI_APPLICATION = 'BackendProjectApi.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'modena_go',
-        'USER': 'luca',
-        'PASSWORD': 'Luke',
-        'HOST': 'localhost',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
