32052d4ff6a4bc20ed8350b8172ca96fced374c6
Luca Zannini <luca.zanninifitstic-edu.com>
2024-01-23 13:13:41 +0100

    Metti app

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import{Routing} from './Componenti/Routing'
-import{LoginPage}from './Componenti/LogInPage'
 import {AppMobile} from './MobileComponenti/AppMobile'
+
 
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-         <AppMobile/>
+         <Routing/>
     </div>
   )
 }
