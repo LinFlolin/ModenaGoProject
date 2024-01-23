af483099c777c008d83c9b2f9cc0d7858efea25d
Luca Zannini <luca.zanninifitstic-edu.com>
2024-01-23 13:36:47 +0100

    fix errori

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,14 @@
 import './App.css'
-<<<<<<< HEAD
-import React, { useState, useEffect } from 'react';
-import {AdminApp} from './AdminComponenti/AdminApp'
-=======
 import{Routing} from './Componenti/Routing'
 import {AppMobile} from './MobileComponenti/AppMobile'
 
 
 import React from 'react'
->>>>>>> luca
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-      {/* <AppMobile/> */}   
-      <AdminApp/>
-=======
          <Routing/>
->>>>>>> luca
     </div>
   )
 }
