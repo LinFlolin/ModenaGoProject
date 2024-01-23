af8c4aadd413a24fd213d672c988524b53524481
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-01-23 15:25:43 +0100

    add com adm

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import React, { useState, useEffect } from 'react';
-import {AdminApp} from './AdminComponenti/AdminApp'
-
+// import {AdminApp} from './AdminComponenti/AdminApp'
+import {AppMobile} from './MobileComponenti/AppMobile'
 const App = () => {
   return (
     <div>
-      {/* <AppMobile/> */}   
-      <AdminApp/>
+      <AppMobile/>   
+      {/* <AdminApp/> */}
     </div>
   )
 }
