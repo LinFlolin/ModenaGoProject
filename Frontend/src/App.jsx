ab6c8d894ecfbc46d38f79abed230b35262bafe0
Luca Zannini <luca.zanninifitstic-edu.com>
2024-01-24 13:17:55 +0100

    risolti problemi in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,29 +1,15 @@
 import './App.css'
-<<<<<<< HEAD
-import{Routing} from './Componenti/Routing'
-import {AppMobile} from './MobileComponenti/AppMobile'
-
-
-import React from 'react'
-=======
 import React, { useState, useEffect } from 'react';
 import {AdminApp} from './AdminComponenti/AdminApp'
-//import{Routing} from './Componenti/Routing'
 import {AppMobile} from './/MobileComponenti/AppMobile'
->>>>>>> main
 
 const App = () => {
   return (
     <div>
-<<<<<<< HEAD
-         <Routing/>
-=======
       <AppMobile></AppMobile>
       {/* <AdminApp/> */}
->>>>>>> main
     </div>
   )
 }
 
 export default App
-
