a5dc6d58033e7963306a633560c51865fed35bd0
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-01-24 12:15:14 +0100

    #Commit to Move To lin branch

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
 import React, { useState, useEffect } from 'react';
 import {AdminApp} from './AdminComponenti/AdminApp'
+import {AppMobile} from './/MobileComponenti/AppMobile'
+
 const App = () => {
   return (
-    <div>      
-      <AdminApp/>
+    <div>
+      <AppMobile></AppMobile>
+      {/* <AdminApp/> */}
     </div>
   )
 }
