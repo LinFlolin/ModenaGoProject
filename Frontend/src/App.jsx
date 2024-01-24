2150ef9de8aed887be00be02fb017d4d67cef8fb
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-01-24 11:23:53 +0100

    RemoveInfoCss

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
 import React, { useState, useEffect } from 'react';
 import {AdminApp} from './AdminComponenti/AdminComponenti/AdminApp'
-// import {AppMobile} from './MobileComponenti/AppMobile'
 const App = () => {
   return (
     <div>
-      {/* <AppMobile/>    */}
+      
       <AdminApp/>
     </div>
   )
