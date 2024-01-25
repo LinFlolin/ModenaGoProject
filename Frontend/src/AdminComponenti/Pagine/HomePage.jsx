151c26c67b82269284b6c9261015472a31f927ef
Giulia Fontana <fontanagiulia6@gmail.com>
2024-01-25 10:58:10 +0100

    Edit some things in mappa.css, add stuff to homepage.jsx but it doesnt show.

--- Frontend/src/AdminComponenti/Pagine/HomePage.jsx
+++ Frontend/src/AdminComponenti/Pagine/HomePage.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../Pagine/PagineCss/HomePage.css'
 
 export const HomePage = () => {
   return (
     <div>
-     
-
+      <div className='firstpart'>
+        <h1>Titolo</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eligendi magni ipsam, reiciendis doloribus quam fugiat laudantium! Modi, optio. Unde voluptatum debitis consequuntur quibusdam qui. Ea enim eligendi rerum reiciendis?</p>
+      </div>
     </div>
    
   )
