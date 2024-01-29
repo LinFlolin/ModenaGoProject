a93bdde3297be704d7254e777ffcf3cf429a8e26
Giulia Fontana <fontanagiulia6@gmail.com>
2024-01-29 17:33:30 +0100

    commit to avoid conflitcs

---
+++ Frontend/src/componenti/Pagine/LuoghiFigo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Axios } from 'axios'
+
+export const LuoghiFigo = () => {
+  return (
+    <div className="container">
+      <h1>My First Bootstrap Page</h1>
+      <p>This is some text.</p>
+    </div> 
+  )
+}
