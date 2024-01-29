d990fc01ffe95f3ff47b3b8c2e7b634a33e20b55
Giulia Fontana <fontanagiulia6@gmail.com>
2024-01-29 17:29:57 +0100

    Added footer and upper chunk to the homepage

--- Frontend/src/AdminComponenti/Pagine/HomePage.jsx
+++ Frontend/src/AdminComponenti/Pagine/HomePage.jsx
@@ -8,6 +8,15 @@ export const HomePage = () => {
         <h1>Titolo</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum eligendi magni ipsam, reiciendis doloribus quam fugiat laudantium! Modi, optio. Unde voluptatum debitis consequuntur quibusdam qui. Ea enim eligendi rerum reiciendis?</p>
       </div>
+      <footer>
+        <ul>
+          <li>Copyright</li>
+          <li>Contattaci</li>
+          <li>Instagram</li>
+          <li>Privacy Policy</li>
+          <li>Patrocinio</li>
+        </ul>
+      </footer>
     </div>
    
   )
