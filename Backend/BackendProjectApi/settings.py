bac0eaa186f1c2cce198eb62d6e8adf9ed7f6ebc
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-02-03 00:04:53 +0100

    commit origin before merge with luca

--- Backend/BackendProjectApi/settings.py
+++ Backend/BackendProjectApi/settings.py
@@ -66,7 +66,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'allauth.account.middleware.AccountMiddleware',
-    
 ]
 
 ROOT_URLCONF = 'BackendProjectApi.urls'
