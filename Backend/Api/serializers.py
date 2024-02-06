c55a6f51dcfc13b1be5bc71c94742e9a179efa1b
Joannes Nicolò Zanfi <nicolo.zanfi@fitstic-edu.com>
2024-02-06 11:28:05 +0100

    Commitdopopullrifare migration

--- Backend/Api/serializers.py
+++ Backend/Api/serializers.py
@@ -8,10 +8,10 @@ UserModel = get_user_model()
 class LuoghiAttrazioneSerialiazer(serializers.ModelSerializer):
     class Meta:
         model = LuoghiAttrazione
-        fields = ('id', 'Titolo', 'Descrizione', 'Sfida')
+        fields = ('id', 'Titolo', 'Descrizione', 'Sfida', 'Immagine')
 
 
 class MapSerializer(serializers.ModelSerializer):
     class Meta:
         model = Mappa
-        fields = ('id', 'Luogo', 'Latitudine', 'Longitudine', 'Immagine')
+        fields = ('id', 'Luogo', 'Latitudine', 'Longitudine')
