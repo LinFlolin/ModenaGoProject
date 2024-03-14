929beee72f6321f82594b043aee1726a3370b04c
Giulia Fontana <fontanagiulia6@gmail.com>
2024-03-14 19:51:23 +0100

    aggiornato il javascript di searchbarjsx E modificato leggermente il css

--- Frontend/src/AdminComponenti/Pagine/SearchBar.jsx
+++ Frontend/src/AdminComponenti/Pagine/SearchBar.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
-import { Component } from 'react'
+import React, {useState} from 'react';
+import { Component } from 'react';
 import CIcon from '@coreui/icons-react';
 import { cilSearch } from '@coreui/icons';
 import '../Pagine/PagineCss/Searchbar.css'
 
-export class SearchBar extends Component {
-    render(){
+export const SearchBar = () => {
+    const [input, setInput] = useState("");
+
+    const fetchData = (value) => (
+        pass 
+        /*QUI METTERE RIFERIMENTO ALL'API DEL BACKEND
+        STESS LAOVRO CHE VA FATTO NELLA LISTA
+    TODO */
+    );
+
+    
         return(
             <div className='searchbarluoghi'>
-            <input type="search" id="luoghi-search" name="q" placeholder='Cosa scoprirai di Modena oggi?'/>
+            <input 
+            type="search" 
+            id="luoghi-search" 
+            name="q" 
+            placeholder='Cosa scoprirai di Modena oggi?' 
+            value={input} 
+            onChange={(e) => setInput(e.target.value)}/>
             <button className='iconaricerca'>
               <CIcon icon={cilSearch} size="lg" className='iconacore' /></button>
             </div>
         )
     }
-}
