ab82d656d3db0e0b7da0cc1399748e85e63819e9
Joannes Nicolò Zanfi <nicolo.zanfi@fitstic-edu.com>
2024-03-16 22:44:00 +0100

    UNFINISHED CHANGES!!!

--- Frontend/src/AdminComponenti/AdminApp.jsx
+++ Frontend/src/AdminComponenti/AdminApp.jsx
@@ -1,32 +1,36 @@
 import React from 'react'
-import { Navbar } from './Pagine/Navbar'
-import{Route,Routes,BrowserRouter} from 'react-router-dom'
-import {HomePage,Info,LuoghiFigo,Sfida,Mappa,Hero,Footer} from './Pagine/index'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import LoginApp from '../LoginComponent/LoginApp'
+import { MarkerCreate } from '../LoginComponent/MarkerCreate'
+import { MarkerDelete } from '../LoginComponent/MarkerDelete'
+import { MarkerDetail } from '../LoginComponent/MarkerDetail'
+import { MarkerList } from '../LoginComponent/MarkerList'
 import RegistrationApp from '../LoginComponent/RegistrationApp'
 import './Admin.css'
-import { DirectionCreate } from '../LoginComponent/DirectionCreate'
-import { PercorsoCreate } from '../LoginComponent/PercorsoCreate'
-import { MarkerCreate } from '../LoginComponent/MarkerCreate'
+import { Navbar } from './Pagine/Navbar'
+import { Footer, Hero, HomePage, Info, LuoghiFigo, Mappa, Sfida } from './Pagine/index'
 export const AdminApp = () => {
   return (
     <div className='adcontainer'>
-      <BrowserRouter> 
-       <Navbar/> 
-       <Hero />  
+      <BrowserRouter>
+        <Navbar />
+        <Hero />
         <Routes>
-          <Route path='/' element={<HomePage/>}/>
-            <Route path='/map' element={<Mappa />} />
-            <Route path='/info' element={<Info/>} />
-            <Route path='/luoghi' element={<LuoghiFigo/>} />
-            <Route path='/sfide' element={<Sfida/>} />
-            <Route path='/login' element={<LoginApp/>}/>
-            <Route path='/registration' element={<RegistrationApp/>}/>
-            <Route path='/Direction' element={<DirectionCreate/>}/>
-            <Route path='/Percorso' element={<PercorsoCreate/>}/>
-            <Route path='/Marker' element={<MarkerCreate/>}/>
-          </Routes>
-          <Footer/>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/map' element={<Mappa />} />
+          <Route path='/info' element={<Info />} />
+          <Route path='/luoghi' element={<LuoghiFigo />} />
+          <Route path='/sfide' element={<Sfida />} />
+          <Route path='/login' element={<LoginApp />} />
+          <Route path='/registration' element={<RegistrationApp />} />
+          {/* <Route path='/Direction' element={<DirectionCreate/>}/>
+            <Route path='/Percorso' element={<PercorsoCreate/>}/> */}
+          <Route path='/add-marker' element={<MarkerCreate />} />
+          <Route path='/delete-marker/:id' element={<MarkerDelete />} />
+          <Route path='/detail-marker/:id' element={<MarkerDetail />} />
+          <Route path='/list-marker' element={<MarkerList />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   )
--- Frontend/src/LoginComponent/MarkerCreate.jsx
+++ Frontend/src/LoginComponent/MarkerCreate.jsx
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import React, { useState } from 'react';
-import { useNavigate } from "react-router-dom";
 import './RegistrationApp.css';
 
 export const MarkerCreate = () => {
