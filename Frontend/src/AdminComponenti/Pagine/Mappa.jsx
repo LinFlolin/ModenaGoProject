458d5b4c368832853f4f5b13cf9162073e43043d
DATCHANI IMNOY <datchani.imnoy@fitstic-edu.com>
2024-03-17 17:26:39 +0100

    non so perchè ma c e il db

--- Frontend/src/AdminComponenti/Pagine/Mappa.jsx
+++ Frontend/src/AdminComponenti/Pagine/Mappa.jsx
@@ -23,7 +23,7 @@ export const Mappa = () => {
             mapTypeControl={false} >
             <Maker/>
           </Map>
-          <Directions/>
+          {/* <Directions/> */}
         </APIProvider>
       </div>
     </div>
